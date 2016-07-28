4037f73e35fe1ff5d90fa51a01b6dcc0fdb8469a
Karina <karisha.babak@gmail.com>
2016-07-29 01:10:06 +0300

    html task

---
+++ Net/Storage/ConfigurationService/ServiceConfig/ServiceElement.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace ConfigurationService
+{
+    /// <summary>
+    /// Describe the element of service
+    /// </summary>
+    public class ServiceElement: ConfigurationElement
+    {
+        /// <summary>
+        /// Get/Set type of service
+        /// </summary>
+        [ConfigurationProperty("serviceType", DefaultValue = "", IsKey = false, IsRequired = true)]
+        public string ServiceType
+        {
+            get { return ((string)(base["serviceType"])); }
+            set { base["serviceType"] = value; }
+        }
+
+        /// <summary>
+        /// Count of services
+        /// </summary>
+        [ConfigurationProperty("login", DefaultValue = "", IsKey = true, IsRequired = false)]
+        public string Login
+        {
+            get { return ((string)(base["login"])); }
+            set { base["login"] = value; }
+        }
+
+        [ConfigurationProperty("address", DefaultValue = "127.0.0.1", IsKey = true, IsRequired = false)]
+        public string Address
+        {
+            get { return ((string)(base["address"])); }
+            set { base["address"] = value; }
+        }
+
+
+        [ConfigurationProperty("port", DefaultValue = "", IsKey = true, IsRequired = true)]
+        public string Port
+        {
+            get { return ((string)(base["port"])); }
+            set { base["port"] = value; }
+        }
+    }
+}
---
+++ Net/Storage/UserStorage/Service/DataUpdatedEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserStorage.Service
+{
+    [Serializable]
+    public class DataUpdatedEventArgs : EventArgs
+    {
+        public User User { get; set; }
+    }
+}
